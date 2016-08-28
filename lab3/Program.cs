02f1546a920bbe134b5fb9345bd837a1ce8a5ae7
Yury Volynets <yury.volynets@gmail.com>
2016-08-28 23:45:35 +0300

    Add project files.

---
+++ lab3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string tocName = "toc.data", fileName = "file.data";
+
+            Console.Write("n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Gen.Generate(tocName, fileName, n);
+
+            FileStream tocFs = new FileStream(tocName, FileMode.Open);
+            BinaryReader tocBr = new BinaryReader(tocFs);
+            FileStream fileFs = new FileStream(fileName, FileMode.Open);
+            BinaryReader fileBr = new BinaryReader(fileFs);
+
+            for (int i = 0; i < n; i++)
+            {
+                switch (tocBr.ReadInt32())
+                {
+                    case 0:
+                        Enrollee e = new Enrollee(fileBr.ReadString(), new DateTime(fileBr.ReadInt32(), fileBr.ReadInt32(), fileBr.ReadInt32()), fileBr.ReadString());
+                        e.Print();
+                        break;
+                    case 1:
+                        Student s = new Student(fileBr.ReadString(), new DateTime(fileBr.ReadInt32(), fileBr.ReadInt32(), fileBr.ReadInt32()), fileBr.ReadString(), fileBr.ReadInt32());
+                        s.Print();
+                        break;
+                    case 2:
+                        Teacher t = new Teacher(fileBr.ReadString(), new DateTime(fileBr.ReadInt32(), fileBr.ReadInt32(), fileBr.ReadInt32()), fileBr.ReadString(), fileBr.ReadString(), fileBr.ReadInt32());
+                        t.Print();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ lab3/Stud.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace lab3
+{
+    abstract class Stud
+    {
+        private string _surname, _faculty;
+        private DateTime _birthday;
+
+        protected Stud(string surname, DateTime birthday, string faculty)
+        {
+            this._surname = surname;
+            this._birthday = birthday;
+            this._faculty = faculty;
+        }
+
+        public void Print()
+        {
+            Console.Write("Surname: {0}; Birthday: {1}; Faculty: {2}", _surname, _birthday.ToShortDateString(), _faculty);
+        }
+
+        public int Age()
+        {
+            return _birthday.DayOfYear < DateTime.Now.DayOfYear ? DateTime.Now.Year - _birthday.Year : DateTime.Now.Year - _birthday.Year - 1;
+        }
+    }
+
+    class Enrollee : Stud
+    {
+        public Enrollee(string surname, DateTime birthday, string faculty) : base(surname, birthday, faculty) { }
+
+        new public void Print()
+        {
+            Console.Write("Enrollee; ");
+            base.Print();
+            Console.WriteLine();
+        }
+    }
+
+    class Student : Stud
+    {
+        private int _course;
+
+        public Student(string surname, DateTime birthday, string faculty, int course)
+            : base(surname, birthday, faculty)
+        {
+            this._course = course;
+        }
+
+        new public void Print()
+        {
+            Console.Write("Student;  ");
+            base.Print();
+            Console.WriteLine("; Course: {0}", _course);
+        }
+    }
+
+    class Teacher : Stud
+    {
+        private string _post;
+        private int _experience;
+
+        public Teacher(string surname, DateTime birthday, string faculty, string post, int experience)
+            : base(surname, birthday, faculty)
+        {
+            this._post = post;
+            this._experience = experience;
+        }
+
+        new public void Print()
+        {
+            Console.Write("Teacher;  ");
+            base.Print();
+            Console.WriteLine("; Post: {0}; Experience: {1}", _post, _experience);
+        }
+    }
+}
